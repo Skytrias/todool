80d419249da4fb1b166ece91bdca4d41851ea667
Paweł Kruszec <pakruszec@gmail.com>
2024-04-02 16:26:49 +0200

    use windows instead of console subsystem on windows

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-odin.exe build src -out:target/todool.exe -thread-count:12 && pushd target && todool.exe && popd
+odin.exe build src -out:target/todool.exe -thread-count:12 -subsystem:windows && pushd target && todool.exe && popd
